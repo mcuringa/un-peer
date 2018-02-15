d1283ca10bb19b5cf384e5a39cc927cd606ec652
mxc <matt@curinga.com>
2018-02-15 20:19:43 -0300

    added status

--- src/challenges/ChallengeEdit.js
+++ src/challenges/ChallengeEdit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from "lodash";
 import dateFormat from 'dateformat';
 import { Link } from 'react-router-dom'
-import {User, Challenge, ChallengeDB} from "./Challenge.js"
+import {User, Challenge, ChallengeDB, ChallengeStatus} from "./Challenge.js"
 import { CalendarIcon, PrimitiveDotIcon } from 'react-octicons';
 
 import FBUtil from "../FBUtil";
@@ -13,7 +13,8 @@ import {
   DatePicker,
   TextAreaGroup,
   StatusIndicator,
-  LoadingSpinner
+  LoadingSpinner,
+  RadioButtonGroup
 } from "../FormUtil";
 
 class ChallengeEditScreen extends React.Component {
@@ -54,6 +55,12 @@ class ChallengeEditScreen extends React.Component {
   }
 
   handleChange(e) {
+    // console.log(e);
+    // console.log(e.target.id);
+    // console.log(e.target.name);
+    // console.log(e.target.type);
+    // console.log(e.target.value);
+
     let c = this.state.challenge;
     c[e.target.id] = e.target.value;
     this.setState({ challenge: c, dirty: true });
@@ -63,8 +70,6 @@ class ChallengeEditScreen extends React.Component {
   handleDateChange(e) {
     let c = this.state.challenge;
     const date = ChallengeDB.parseDateControlToUTC(e.target.value);
-    console.log("date changed");
-    console.log(date);
     c[e.target.id] = date;
 
     this.setState({ challenge: c, dirty: true });
@@ -72,21 +77,51 @@ class ChallengeEditScreen extends React.Component {
     this.save();
   }
 
-
-
   render() {
     const c = this.state.challenge;
+    const ts = (d)=>dateFormat(d,"yyyy-mm-dd HH:MM:ss");
+    const statusOptions = { };
+    statusOptions[ChallengeStatus.DRAFT] = "draft";
+    statusOptions[ChallengeStatus.REVIEW] = "review";
+    statusOptions[ChallengeStatus.PUBLISHED] = "published";
+    statusOptions[ChallengeStatus.ARCHIVED] = "archived";
+    
+
     return (
-      <div>
-        <h3 className="">Challenge Edit Form
-          <StatusIndicator dirty={this.state.dirty} loading={this.state.loading} /></h3>
+      <div className="ChallengeEdit screen card bg-light">
+        <div className="card-header">
+          <div className="row">
+            <div className="col-11">{c.title}</div>
+            <div className="col-1">
+              <StatusIndicator dirty={this.state.dirty} loading={this.state.loading} />
+            </div>
+          </div>
+          <div className="small text-muted">Owner: {this.state.owner.first} {this.state.owner.last}</div>
+          <small className="text-muted"><tt>created: {ts(c.created)} | </tt></small>
+          <small className="text-muted"><tt>modified: {ts(c.modified)}</tt></small>
+        </div>
         <form>
+
           <TextGroup id="title"
             value={c.title} 
             label="Challenge Title" 
             onChange={this.handleChange} 
             required={true} />
           
+          <div className="form-group">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">Set Status</span>
+              </div>
+              <select id="status" value={c.status} className="custom-select" onChange={this.handleChange}>
+                <option value={ChallengeStatus.DRAFT}>draft</option>
+                <option value={ChallengeStatus.REVIEW}>review</option>
+                <option value={ChallengeStatus.PUBLISHED}>published</option>
+                <option value={ChallengeStatus.ARCHIVED}>archive</option>
+              </select>
+            </div>
+          </div>
+
           <TextAreaGroup id="prompt"
             value={c.prompt}
             label="Description"
