020d7de9fe1aae4170d806eca4e98ed46deee650
Nik Nyby <nikolas@gnu.org>
2018-03-03 17:44:03 -0500

    bookmarks mock basics

--- src/BookmarksScreen.js
+++ src/BookmarksScreen.js
@@ -1,27 +1,76 @@
 import React from "react";
 import _ from "lodash";
-import Bookmarks from "react-calendar";
-import {ChallengeDB, ChallengeStatus} from "./challenges/Challenge.js"
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
+import df from './DateUtil';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 class BookmarksScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bookmarks: []
+      bookmarks: [
+        {
+          title: 'Strategic Management',
+          posted_on: new Date('2018-02-01'),
+          owner: 'Sam Thorndike',
+          responses: [
+            {
+              rating: 3
+            },
+            {
+              rating: 4
+            },
+            {
+              rating: 3
+            }
+          ]
+        },
+        {
+          title: 'Internal Communication',
+          posted_on: new Date('2018-01-24'),
+          owner: 'Alice Li',
+          responses: [
+            {
+              rating: 3
+            },
+            {
+              rating: 4
+            }
+          ]
+        }
+      ]
     };
   }
 
-  componentWillMount() {
-    ChallengeDB.findByStatus(ChallengeStatus.PUBLISHED)
-      .then((t) => {
-        this.setState({challenges: t})
-      });
-  }
-
   render() {
+    const items = this.state.bookmarks.map((i) => {
+
+      const responses = i.responses.map((r) => {
+        return <div>{r.rating} stars</div>;
+      });
+
+      return <AccordionItem>
+        <AccordionItemTitle>
+        <div>Posted on {df.df(i.posted_on)}</div>
+        <h3>{i.title}</h3>
+        <h5>Challenge owner: {i.owner}</h5>
+        </AccordionItemTitle>
+        <AccordionItemBody>
+        {responses}
+        </AccordionItemBody>
+        </AccordionItem>;
+    });
+
     return (
       <div className="bookmarks-view">
-          <h1>Bookmarks!</h1>
+          <Accordion>
+              {items}
+          </Accordion>
       </div>
     );
   }
