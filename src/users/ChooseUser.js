cb20b8b449cff6fadca5956e613d5cd26eaed95c
mxc <matt@curinga.com>
2018-03-05 19:03:28 -0300

    added a generic text control that searches through all users and returns a selectable list.

---
+++ src/users/ChooseUser.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import _ from "lodash";
+import db from "../DBTools";
+
+import {TextInput} from "../FormUtil"
+import $ from "jquery";
+
+
+class ChooseUser extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.termKey = _.uniqueId("term");
+
+    this.state = {
+      users: [],
+      [this.termKey]: ""
+    };
+
+    this.onChange = _.bind(this.onChange, this);
+  }
+
+  componentWillMount() {
+    db.findAll("/users").then((t)=>{
+      this.setState({
+        users: t,
+      });
+    });
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    this.setState({[e.target.id]: e.target.value});
+  }
+
+  render() {
+
+    if(this.props.hide)
+      return null;
+
+    const term = this.state[this.termKey];
+    const search = ()=> {
+      if(!term || term.length == 0)
+        return [];
+      
+      const f = (u)=> {
+        const s = (u.email + u.firstName + u.lastName).toLowerCase();
+        return s.includes(term.toLowerCase());
+      }
+
+      return _.filter(this.state.users, f);
+    }
+
+    const results = search();
+    const clearAndSelect = (u)=> {
+      console.log("clear called")
+      console.log(u.email);
+      this.props.selectUser(u);
+      this.setState({[this.termKey]: ""});
+    }
+
+
+    if(results.length > 0)  
+      $(`#${this.termKey}_results`).show();
+
+    return (
+      <div>
+        <input className="form-control" data-toggle="dropdown" 
+          type="text" 
+          id={this.termKey} 
+          onChange={this.onChange} 
+          placeholder={this.props.placeholder} />
+        <SearchResults userList={results} 
+          id={`${this.termKey}_results`} 
+          selectUser={clearAndSelect} />
+      </div>
+    );
+  }
+}
+
+const SearchResults = (props)=> {
+
+  if(_.size(props.userList) == 0)
+    $(`#${props.id}`).hide();
+
+  const makeRow = (u)=> {
+    // console.log("making a row");
+    // console.log(u.email);
+
+    const choose = ()=>{ 
+      console.log("choose called")
+      console.log(u.email);
+      props.selectUser(u);
+      $(`#${props.id}`).hide();
+    }
+
+    return (
+      <div id={props.id} key={`${props.id}_${u.uid}`} className="dropdown-item">
+        <button type="button" className="btn btn-link" onClick={choose}>
+          {u.firstName} {u.lastName} &lt;{u.email}&gt;
+        </button>
+      </div>
+    )
+  }
+
+  const results = _.map(props.userList, makeRow);
+
+  return (
+    <div id={props.id} className="dropdown-menu">{results}</div>
+  );
+
+}
+
+export default ChooseUser;
