bbc4f05299e8741c5247c136d4bdfb5b876b7b57
mxc <matt@curinga.com>
2018-03-06 12:04:24 -0300

    test cases for bookmarking calls to firestore

--- src/users/Users.test.js
+++ src/users/Users.test.js
@@ -23,6 +23,46 @@ function rp() {
   return p;
 }
 
+it("should add a bookmark for a user",()=>{
+
+  const uid = "qeNXoRsAlsVniTfGy1wHKMHpLIV2";
+  const c = {
+    id: "what-about-the-careless-staff",
+    title: "What about the careless staff?",
+    start: new Date("2018-02-26T21:21:35.746Z"),
+  };
+  let o = {
+    avgRating: 3.5,
+    created: new Date("2018-02-26T21:21:35.746Z"),
+    id: "9hJ3ccqz6aSaFwUPkDUXU2AbKX02",
+    modified: new Date("2018-03-03T04:03:43.133Z"),
+    text: "It’s a test",
+    title: "Test response from Jingbo"
+  }
+
+  return UserDB.addBookmark(uid, o, c).then((b)=>{
+      expect(b).toBeDefined();
+      expect(b.challengeId).toBe(c.id);
+    });
+
+});
+
+
+it("should find all bookmarks for a user",()=>{
+  const uid = "qeNXoRsAlsVniTfGy1wHKMHpLIV2";
+  return db.findAll("/users/qeNXoRsAlsVniTfGy1wHKMHpLIV2/bookmarks").then((bookmarks)=>{
+      expect(bookmarks).toBeDefined();
+    });
+});
+
+it("should delete a user's bookmarks",()=>{
+  const uid = "qeNXoRsAlsVniTfGy1wHKMHpLIV2";
+  const bookmarkId = "9hJ3ccqz6aSaFwUPkDUXU2AbKX02";
+  return db.delete(`/users/${uid}/bookmarks`, bookmarkId).then(()=>{});
+});
+
+
+
 
 it.skip("should create a new user in auth and DB",()=>{
   const user = {
