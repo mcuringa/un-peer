8073e9e08372f18861568e3ac37d511ec1cf8e1b
mxc <matt@curinga.com>
2018-03-07 12:44:36 -0300

    updated formatting, added last login

--- src/users/ProfileScreen.js
+++ src/users/ProfileScreen.js
@@ -4,17 +4,18 @@ import _ from "lodash";
 import {StatusIndicator} from "../FormUtil";
 
 import Modal from "../Modal";
+import {ChallengeDB} from "../challenges/Challenge.js";
 
 
 class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
-    // const firebase = FBUtil.init();
-    // let fbu = firebase.auth().currentUser;
+
     let user = {
       email: props.user.email, 
       displayName: `${props.user.firstName} ${props.user.lastName}`, 
-      uid: props.user.uid};
+      uid: props.user.uid
+    };
 
     this.state = {
       "loading": false,
@@ -26,6 +27,13 @@ class ProfileScreen extends React.Component {
     this.sendPass = this.sendPass.bind(this);
     this.save = this.save.bind(this);
     this.save = _.debounce(this.save, 2000);
+  }
+
+  componentWillMount() {
+    const firebase = FBUtil.init();
+    let fbu = firebase.auth().currentUser;
+    this.setState({lastLogin:fbu.metadata.lastSignInTime});
+
   }
   
   sendPass(e) {
@@ -83,24 +91,30 @@ class ProfileScreen extends React.Component {
     return (
       <div className="ProfileScreen screen">
         <div className="row">
-          <div className="col-11 text-strong">Your profile</div>
+          <div className="col-11"><h2>Your profile</h2></div>
           <div className="col-1">
             <StatusIndicator dirty={this.state.dirty} loading={this.state.loading} />
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-4 text-strong">Name:</div>
+          <div className="col-sm-3 text-right"><strong>Name:</strong></div>
           <div className="col">{`${this.props.user.firstName} ${this.props.user.lastName}`}</div>
         </div>
         <div className="row">
-          <div className="col-sm-4 text-strong">Email:</div>
+          <div className="col-sm-3 text-right"><strong>Email:</strong></div>
           <div className="col">{this.props.user.email}</div>
         </div>
-        <button type="button" onClick={this.sendPass} className="btn btn-secondary mr-2">Reset Password</button>
-        
-        <button type="button" onClick={this.signout} className="btn btn-secondary">
-            Sign Out
-        </button>
+        <div className="row">
+          <div className="col-sm-3 text-right"><strong>Last login:</strong></div>
+          <div className="col">{this.state.lastLogin}</div>
+        </div>
+        <div className="mt-2 d-flex justify-content-end">
+          <button type="button" onClick={this.sendPass} className="btn btn-secondary mr-2">Reset Password</button>
+          
+          <button type="button" onClick={this.signout} className="btn btn-secondary">
+              Sign Out
+          </button>
+        </div>
         <Modal id="ResetModal"
           show={this.state.reset} 
           body="Please check your email for instructions on resetting your password."/>
