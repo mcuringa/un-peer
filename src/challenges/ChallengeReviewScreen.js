0d53ef0a2255c59cb74fdb823b3f078aae013305
mxc <matt@curinga.com>
2018-03-14 12:07:50 -0300

    added hash style to unrated stars

--- src/challenges/ChallengeReviewScreen.js
+++ src/challenges/ChallengeReviewScreen.js
@@ -465,6 +465,12 @@ const StarGradient = (props)=> {
   return (
     <div style={{height: 0}}>
     <svg>
+      <pattern id="UnratedHash" patternUnits="userSpaceOnUse" width="4" height="4">
+        <path d="M-1,1 l2,-2
+                 M0,4 l4,-4
+                 M3,5 l2,-2" 
+              style={{stroke:"black", strokeWidth:1}} />
+      </pattern>
       <linearGradient id="QuarterFull" {...angles}>
         <stop offset="40%" stopColor="#6c757d"/>
         <stop offset="40%" stopColor="white"/>
