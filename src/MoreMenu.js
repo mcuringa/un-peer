e41c052b154e7816264871bac15fa7d0c469e8c6
mxc <matt@curinga.com>
2018-03-28 19:28:31 -0300

    ran into firebase problem

---
+++ src/MoreMenu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {GearIcon, TriangleDownIcon, ThreeBarsIcon} from "react-octicons";
+
+const MoreMenu = (props)=> {
+  const MenuIcon = ()=> {
+    if(!props.menuIcon)
+      return <KebabVerticalIcon />;
+    if(props.menuIcon === "gear")
+      return <GearIcon />
+    if(props.menuIcon === "arrow")
+      return <TriangleDownIcon />
+    if(props.menuIcon === "bars")
+      return <ThreeBarsIcon />
+
+    return props.menuIcon;
+  }
+
+  const MenuLabel = ()=> {
+    if(!props.label)
+      return null;
+    return (<span className="mr-1">{props.label}</span>)
+  }
+
+  const dir = props.direction || "dropleft";
+
+  return (
+    <div className={`MoreMenu dropdown ${dir}`}>
+      <button type="button" className="MoreIcon btn btn-link text-dark m-0 p-0 icon-secondary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <MenuLabel /><MenuIcon />
+      </button>
+      <div className="dropdown-menu mt-0 pt-0 dropdown-menu-right">
+        {props.children}
+      </div>
+    </div>
+  )
+
+}
+
+
+const KebabVerticalIcon = ()=> {
+
+  const len = 32;
+  return (
+    <svg width={len} height={len} className="octicon octicon-kebab-vertical" viewBox={`0 0 ${len} ${len}`}>
+      <circle className="icon-bg icon-light" cx={len/2} cy={len/2} r={(len/2)} />
+      <circle cx={len/2} cy={len * .33} r={1.5} />
+      <circle cx={len/2} cy={len/2} r={1.5}  />
+      <circle cx={len/2} cy={len * .66} r={1.5} />
+    </svg>
+  )
+}
+
+      // <path fill-rule="evenodd" d="M0 2.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm0 5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm0 5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z"></path>
+
+export default MoreMenu;
+export {KebabVerticalIcon}
